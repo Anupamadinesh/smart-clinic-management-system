0155363ae2fe310a92e401192b1ff171e7a8dd55
Anupamadinesh <anupamadinesh31@gmail.com>
2026-02-21 13:49:26 +0530

    Refactor DoctorService to use DoctorRepository
    Refactored DoctorService to use DoctorRepository for data operations. Added methods to get, create, update, and delete doctors.

--- src/main/java/com/project/back_end/services/DoctorService.java
+++ src/main/java/com/project/back_end/services/DoctorService.java
@@ -1,18 +1,57 @@
 package com.project.back_end.services;
 
 import com.project.back_end.models.Doctor;
-import java.util.ArrayList;
-import java.util.List;
+import com.project.back_end.repositories.DoctorRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class DoctorService {
 
-    private List<Doctor> doctors = new ArrayList<>();
+    private final DoctorRepository doctorRepository;
 
-    public List<Doctor> getAllDoctors() {
-        return doctors;
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
     }
 
-    public void addDoctor(Doctor doctor) {
-        doctors.add(doctor);
+    // Get all doctors
+    public List<Doctor> getAllDoctors() {
+        return doctorRepository.findAll();
+    }
+
+    // Get doctor by ID
+    public Doctor getDoctorById(Integer id) {
+        Optional<Doctor> doctor = doctorRepository.findById(id);
+        return doctor.orElse(null);
+    }
+
+    // Add new doctor
+    public Doctor createDoctor(Doctor doctor) {
+        return doctorRepository.save(doctor);
+    }
+
+    // Update doctor
+    public Doctor updateDoctor(Integer id, Doctor updatedDoctor) {
+        Optional<Doctor> existingDoctor = doctorRepository.findById(id);
+
+        if (existingDoctor.isPresent()) {
+            Doctor doctor = existingDoctor.get();
+            doctor.setName(updatedDoctor.getName());
+            doctor.setSpecialization(updatedDoctor.getSpecialization());
+            doctor.setPhone(updatedDoctor.getPhone());
+            return doctorRepository.save(doctor);
+        }
+        return null;
+    }
+
+    // Delete doctor
+    public String deleteDoctor(Integer id) {
+        if (doctorRepository.existsById(id)) {
+            doctorRepository.deleteById(id);
+            return "Doctor deleted successfully";
+        }
+        return "Doctor not found";
     }
 }
