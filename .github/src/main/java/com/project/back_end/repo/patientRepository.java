21155fa85fcbae3ab736f2a96929bed85315014f
Anupamadinesh <anupamadinesh31@gmail.com>
2026-02-21 14:07:23 +0530

    Refactor PatientRepository package and imports

--- .github/src/main/java/com/project/back_end/repo/patientRepository.java
+++ .github/src/main/java/com/project/back_end/repo/patientRepository.java
@@ -1,1 +1,12 @@
-ew
+package com.project.back_end.repositories;
+
+import com.project.back_end.models.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Integer> {
+
+    // Custom query method (optional)
+    Patient findByEmail(String email);
+}
